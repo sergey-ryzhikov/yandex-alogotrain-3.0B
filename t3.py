1a1beee06e9e83fa3aea10741b1a123631a7fcfb
Sergey Ryzhikov <serg.ryzh.ml@gmail.com>
2023-02-22 21:57:27 +0300

    solved t3

--- t3.py
+++ t3.py
@@ -9,47 +9,52 @@ assert len(targs) == k, "wrong length (targs)"
 
 svals = list(sorted(set(vals)))
 
-# def binsearch(slist, item):
-#     """ Returns item index or None """
-#     low = 0
-#     high = len(slist) - 1
+def binsearch_less(slist, item):
+    """ Returns leftmost item index in sorted list
+        which is smaller then item
+        or None.
+    """
 
-#     if high == -1:  # empty slist
-#         return None
+    if not len(slist):
+        return None
 
-#     while high != low:
-#         mid = (low + high) // 2
+    left, right = 0, len(slist)  # [left, right)
+    prev_mid = 0
 
-#         if slist[mid] < item:  # right
-#             low = mid 
-#         else:  # left
-#             high = mid
-        
-#         print(low, high)
+    while left != right:
+        mid = (left + right) // 2
+        if mid == prev_mid:
+            break
 
-#     if slist[low] != item:
-#         return None
-#     else:
-#         return low
+        if slist[mid] >= item:
+            right = mid
+        else:
+            left = mid
 
-# print(binsearch([1], 1))
-# print(binsearch([1,2,3], 2))
-# # print(binsearch([1,2,3,4,5], 6))
-# # print(binsearch([1,2,3,4,5], 0))
-# # print(binsearch([1,2,3,4,5], 3))
+        # print(f"{left=}, {right=}, {mid=}: {slist[mid]} : {slist=}, {item=}")
+        prev_mid = mid
+
+    if slist[left] >= item:
+        return None
+    else:
+        return left
+
+# func = binsearch_less
+# print(func([1], 2), 0)
+# print(func([1,2,3], 2), 0)
+# print(func([1,2,3,4,5], 6), 4)
+# print(func([1,2,3,4,5], 0), None)
+# print(func([1,2,3,4,5], 1), None)
+# print(func([1,2,3,4,5], 2), 0)
+# print(func([1,2,4,5,6], 3), 1)
 
 for targ in targs:
-    itop = None
-
-    for i,v in enumerate(svals):
-        if v >= targ:
-            break
-        else:
-            itop = i
+    itop = binsearch_less(svals, targ)
 
     if itop is not None:
         cnt = 1 + itop  # index in list is a number of smaller values
     else:
         cnt = 0
+
     print(cnt)
     
