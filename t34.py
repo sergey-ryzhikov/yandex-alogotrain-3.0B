83fb00d6de0679443a3e6eb56d7d0f3ee9e11b06
Sergey Ryzhikov <serg.ryzh.ml@gmail.com>
2023-03-09 05:15:37 +0300

    t34 solved

--- t34.py
+++ t34.py
@@ -2,7 +2,7 @@ import sys
 
 nvert, nedg = map(int, input().split())
 
-graph = {}
+graph = {x:[] for x in range(1, nvert+1)}
 for line in sys.stdin.readlines():
     line = line.strip()
     if not line:  # skip empty line
@@ -10,9 +10,53 @@ for line in sys.stdin.readlines():
     a,b = line.split()
     a = int(a); b = int(b)
     graph.setdefault(a, []).append(b)
-    graph.setdefault(b, [])  # for max()
+    graph.setdefault(b, [])
 
 
 def solve(graph):
-    """ Topological sorting """
+    """ Topological sorting + loop detection. """
+    if not graph:
+        return []
     
+    visited = [None] * (max(graph.keys()) + 1)  # None, 1, 2
+    ret = []
+
+    for k in graph:
+        if visited[k] is not None:
+            continue
+        stack = [k]
+        while stack:
+            s = stack.pop()
+            if visited[s] is None:
+                visited[s] = 1
+
+            for child in graph[s]:
+                vc = visited[child]
+
+                if vc is None:
+                    stack.append(s)
+                    stack.append(child)
+                    visited[child] = 1
+                    break
+                elif vc == 1:  # already in path
+                    # if s == child:  # loop
+                    #     pass
+                    # else:
+                    #     # Cycle found
+                        return None
+                elif vc == 2:
+                    pass
+                else:
+                    raise ValueError(f"{vc=}")
+            else:
+                # finished or no children
+                ret.append(s)
+                visited[s] = 2
+            # print(f"{stack=}", file=sys.stderr)
+    return ret
+
+# print(f"{graph=}", file=sys.stderr)
+ans = solve(graph)
+if ans is None:
+    ans = [-1]
+print(*reversed(ans))
